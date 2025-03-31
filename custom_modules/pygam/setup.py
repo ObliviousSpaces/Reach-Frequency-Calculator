22a706ab9fc4d13177e972133e3a453d240fb287
ObliviousSpaces <parisi.john87@gmail.com>
2025-03-31 15:16:46 -0400

    Update setup.py

--- custom_modules/pygam/setup.py
+++ custom_modules/pygam/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pygam',
+    name='pygam2',
     version='0.1.0',  # Adjust the version as needed
     packages=find_packages(),  # This will automatically find the Python modules in the directory
     install_requires=[  # Add any dependencies your module might need
